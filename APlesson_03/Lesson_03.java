43a93c1dbd7af926bd986361b14d5197bf841dd4
NickW12 <123naw@gmail.com>
2016-09-09 11:50:30 -0700

    completed lesson 3

---
+++ APlesson_03/Lesson_03.java
@@ -0,0 +1,63 @@
+import java.util.Scanner; //import Statement
+
+public class Lesson_03
+{
+	public static void main(String[]args)
+	{
+		//instantiate the new scanner object "ketboard"
+		Scanner keyboard = new Scanner(System.in);
+		
+		//prompt for user input
+		System.out.println("What is your name? ");
+		
+		String name = keyboard.next();
+		
+		System.out.println("Wow!" + name + " cool name");
+		
+		//prompt for user input
+		System.out.println("How old are you? ");
+		
+		//search for the next integer that the user enters
+		int num = keyboard.nextInt();
+		
+		//prompt for user input
+		System.out.println("Who is your favorite teacher? ");
+		
+		String teacher = keyboard.next();
+		
+		System.out.println("Okay! " + teacher + " is very good");
+		
+		//prompt for user input
+		System.out.println("What do you do for fun? ");
+		
+		String sport = keyboard.next();
+		
+		System.out.println("cool! " + sport + " is very good");
+		
+		//prompt for user input
+		System.out.println("What kind of music do you like? ");
+		
+		String music = keyboard.next();
+		
+		System.out.println("cool! " + music + " is a good artist");
+		
+		//prompt for user input
+		System.out.println("How may siblings do you have? ");
+		
+		String siblings = keyboard.next();
+		
+		System.out.println("cool! " + siblings + " One sister");
+		
+		//prompt for user input
+		System.out.println("What do you want to be when you grow up? ");
+		
+		String videoediting = keyboard.next();
+		
+		System.out.println("Wow!" + videoediting + " sounds fun");
+	
+		//print the results...
+		System.out.println("Wow " + num + " is perfect. You're pretty cool. " );
+		
+		
+	}
+}
