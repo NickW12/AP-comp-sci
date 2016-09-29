a9e15b96411b598f3e7c943c551a89c93993f3f2
NickW12 <123naw@gmail.com>
2016-09-29 11:30:42 -0700

    Added lesson 2

---
+++ APlesson_02/AddressLabel.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AddressLabel
+{
+	public static void main(String[]args)
+	{
+		String fname = "Nick";
+		String lname = "Wallace";
+		String addressline1 = "2345 Something Street";
+		String addressline2 = "san Diego CA 923820";
+		String addressline3 = "ADDRESS LINE 3";
+		
+		System.out.println(fname + " " + lname);
+		System.out.println(addressline1);
+		System.out.println(addressline2);
+		System.out.println(addressline3);
+		
+		
+	}
+}
---
+++ APlesson_04/Compound interest.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Compound_interest
+{
+	public static void main(String[]args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter your rate");
+		double rate = nextDouble();
+		System.out.println("Enter your principal");
+		double principal = nextDouble();
+		System.out.println("Enter your time");
+		double time = nextDouble();
+		System.out.println("Enter your number");
+		double number = nextDouble();
+		
+		
+		System.out.println("Your monthly payment will be" + payment(rate, principal, number, time));
+		
+	}
+	
+	public double payment(double r, double p, double n, double t)
+	{
+		Math.pow(number, exponent value);
+	}
+		
+		
+	}
+}
