988749f65a523e2e17573aeef637b0b208c58f1d
NickW12 <123naw@gmail.com>
2016-10-07 11:46:15 -0700

    Circle43 Added

--- APlesson_04/Circle43.java
+++ APlesson_04/Circle43.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
-public class Circle
+public class Circle43
 {
-	static double r;
-	
 	public static void main(String[]args)
 	{
 		Scanner input = new Scanner(System.in);
 		System.out.println("What is radius");
-		r = input.nextDouble();
-		calcArea();
-		print();
+		 double r = input.nextDouble();
+		print(r, calcArea(r));
 	}
-	public static void calcArea()
+	public static double calcArea(double r)
 	{
-		area = 3.14*(r*r);
+		return 3.14*(r*r);
 	}
-	public static void print()
+	public static void print(double r, double area)
 	{
 		System.out.println("The area of the circle is " + area + "in lengh is" +r);
 	}
