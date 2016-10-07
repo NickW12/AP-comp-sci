f08bf4aa45249319b112ea583acbc0b2dfa12f3e
NickW12 <123naw@gmail.com>
2016-10-07 11:31:27 -0700

    Cube43 Added

---
+++ APlesson_04/Cube43.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Cube43
+{
+	static double side;
+	static double sa;
+	
+	public static void main(String[]args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is the side");
+		side = input.nextDouble();
+		calcSurf(); 
+		print();
+		
+	}
+	public static void calcSurf()
+	{
+		sa = side*side*6;
+	}
+	public static void print()
+	{
+		System.out.println("The surface area of the cube whose sides are " + side + "in lengh is" +sa);
+	}
+}
