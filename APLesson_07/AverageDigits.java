5a2bf401c38676d9a59b0b9e830804f6e5f33d61
NickW12 <123naw@gmail.com>
2016-11-10 10:52:52 -0800

    Added Avg Digits

---
+++ APLesson_07/AverageDigits.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class AverageDigits
+{	
+
+	static int number;
+	
+	static double average = 0;
+	
+	static int digits = 0;
+
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);	
+		
+		System.out.println("Enter a number");
+		
+		number = kb.nextInt();
+		
+		avgDigits();
+		System.out.println("The average of " + number + " is " + average);
+
+			
+	}	
+
+	
+	public static void avgDigits()
+	{
+		int num = number;
+		while (num > 0)
+		{
+			digits++;
+			average = average + (number%10);
+			num/= 10;
+		}
+		average = average / digits;
+	}		 
+}
