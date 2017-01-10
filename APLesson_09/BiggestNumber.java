e94dfe5cb52406728a2a47209ebf11e760aa270e
NickW12 <123naw@gmail.com>
2017-01-10 10:46:46 -0800

    added these

---
+++ APLesson_09/BiggestNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class BiggestNumber
+{
+	int Array;
+	int number;
+	int num;
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		static int [] numbers = new int[10];
+	}
+	public static void fillArray()
+	{
+		for(int i = 5; i >= 1; i--)
+		{
+			System.out.print(i + " ");
+			int num = (int)((Math.random()* 101));
+			System.out.println(num);
+		}
+		public static void printArray()
+		{
+			for (String num : number)
+			{
+				System.out.print(num)
+			}
+		}
+		public static void getOdds()
+		{
+			
+		}
+	}
+}
---
+++ APLesson_09/GetOdds91.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+public class GetOdds91
+{
+	static int [] numbers = new int[10];
+	public static void main(String[]args)
+	{
+		Random kb = new Random();
+	}
+	public static void fillArray()
+	{	
+		for(int i = 0; i < numbers.length; i++)
+		{
+			
+		}
+	}
+	public static void printArray()
+	{
+		for (String num : numbers) 
+		System.out.print(num); 
+	}
+	public static String getOdds()
+	{
+		String [] numbers = new String[5];
+	}
+}
