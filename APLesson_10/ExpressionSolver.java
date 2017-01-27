e65908eb534e57dc7b3d50ea51884989315a5dab
123nwa@gmail.com <Sealy007>
2017-01-27 13:26:58 -0800

    FIxed The expressionsolver

--- APLesson_10/ExpressionSolver.java
+++ APLesson_10/ExpressionSolver.java
@@ -1,19 +1,56 @@
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
-
-public class ExpressionSolver
+public class expressionsolver
 {
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Please enter an equatiion");
+		System.out.println("Enter an expression ");
 		String expression = kb.nextLine();
+		ArrayList<String>equation = new ArrayList<>(Arrays.asList(expression.split(" ")));
+		System.out.println(expression);
+		System.out.println(equation);
+		doEquation(equation);
 	}
-	public static void deEquation(ArrayList)
+	public static void doEquation(ArrayList<String> equation)
 	{
-		i < equation
-		if equation = a
-		else i (i-1)(i+1)
+		int i = 0;
+		while (i < equation.size()){
+			if  (equation.get(i).equals("*") || equation.get(i).equals("/"))
+			{
+				if(equation.get(i).equals("*"))
+				{
+					equation.set(i,""+(Integer.parseInt(equation.get(i-1))*Integer.parseInt(equation.get(i+1))));
+				}
+				else
+				{
+					equation.set(i,""+(Integer.parseInt(equation.get(i-1))/Integer.parseInt(equation.get(i+1))));
+				}
+				equation.remove(i-1);
+				equation.remove(i);
+			}
+			else
+			i++;
+		}
+		i = 0;
+		while(i < equation.size())
+		{
+		if( equation.get(i).equals("+") || equation.get(i).equals("-"))
+			{
+		if(equation.get(i).equals("+"))
+				{
+		equation.set(i,"" + (Integer.parseInt(equation.get(i-1))  +  Integer.parseInt(equation.get(i+1))));
+				}
+		else{
+		equation.set(i,"" + (Integer.parseInt(equation.get(i-1))  -  Integer.parseInt(equation.get(i+1))));
+				}
+		equation.remove(i-1);
+		equation.remove(i);
+			}
+			else 
+			i++;
+		}
+		System.out.println(equation);
 	}
-	
+}
--- APLesson_10/keepcomposites.java
+++ APLesson_10/keepcomposites.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Keepcomposites
+public class keepcomposites
 {
 	public static void main(String[]args)
 	{
